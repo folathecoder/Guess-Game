e8e88d7598fef8fffdf37222101632395d232923
folathecoder <officialakinloyefolarin@gmail.com>
2021-04-21 11:50:01 +0100

    First Iteration

--- script.js
+++ script.js
@@ -2,13 +2,61 @@
 
 // ============= START PROJECT ===============
 
-document.querySelector('.check').addEventListener('click', function () {
-    // =================== 
-    // Displays The Number(s) Guessed By A User
-    // Convert Value To Number
-    // ===================
-    const guess = Number(document.querySelector('.guess').value);
-    // ===== Output the Guessed Number(s)
-    console.log(guess, typeof guess);
+// Enable any value appear in the console on click of the "Check!" button
 
-}); 
+const check = document.querySelector('.check');
+
+const message = document.querySelector('.message');
+
+const bodyBg = document.querySelector('.body-bg');
+
+// const body = document.getElementsByTagName('body');
+
+// Array.from(body).forEach(
+//     function (e) {
+//         console.log(e);
+//     }
+// )
+
+// const score = document.querySelector('.score').textContent;
+// const gameScore = Number(score);
+// console.log(gameScore, typeof(gameScore));
+
+check.addEventListener('click', function (e) {
+
+    const guess = document.querySelector('.guess').value;
+
+    // const score = document.querySelector('.score').text;
+
+    console.log(guess);
+
+
+
+    const score = document.querySelector('.score').textContent;
+    const gameScore = Number(score);
+    console.log(gameScore, typeof(gameScore));
+
+
+
+
+
+    if (!guess) {
+        console.log(`Not a number!`);
+
+        message.textContent = `Oopps!🤷‍♂️...Not a number!`;
+
+        const currentScore = gameScore - 1;
+
+        console.log(currentScore);
+
+
+    }
+    else {
+        message.textContent = `Nice👌, try again!`;
+
+        const currentScore = gameScore + 1;
+
+        console.log(currentScore);
+
+    }
+})
