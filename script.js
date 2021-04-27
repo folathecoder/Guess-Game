19eaea8e2b42466570c1e0191680563436f68c0c
folathecoder <officialakinloyefolarin@gmail.com>
2021-04-27 10:56:16 +0100

    Game Logic ==== Score Change On Guessing Right or Wrong

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 const number = document.querySelector(".number");
 number.textContent = secretNumber;
 
+// Assign initial score value
+
+let score = 20;
+
 // Enable any value appear in the console on click of the "Check!" button
 
 const check = document.querySelector('.check');
 
-check.addEventListener('click', function(e) {
+check.addEventListener('click', function (e) {
 
     const guessedNumber = Number(document.querySelector('.guess').value);
 
@@ -23,12 +27,22 @@ check.addEventListener('click', function(e) {
     }
     else if (guessedNumber === secretNumber) {
         message.textContent = `🎉 You guessed right!`;
+
+        // Increase the score by 1
+        score = score + 1;
+        document.querySelector('.score').textContent = score;
     }
     else if (guessedNumber > secretNumber) {
         message.textContent = `😢 Too high!`;
+        // Decrease the score by 1
+        score = score - 1;
+        document.querySelector('.score').textContent = score;
     }
     else if (guessedNumber < secretNumber) {
         message.textContent = `😢 Too low!`;
+        // Decrease the score by 1
+        score = score - 1;
+        document.querySelector('.score').textContent = score;
     }
 
 
