ae4312a8ace3813306098f1a7e6f10bde92094a7
folathecoder <officialakinloyefolarin@gmail.com>
2021-04-27 02:31:13 +0100

    Game Logic ==== Generated Random Numbers & Compared To Guessed Number

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 // ============= START PROJECT ===============
 
+// Implement randomly generated numbers
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const number = document.querySelector(".number");
+number.textContent = secretNumber;
+
 // Enable any value appear in the console on click of the "Check!" button
 
 const check = document.querySelector('.check');
@@ -15,8 +21,15 @@ check.addEventListener('click', function(e) {
     if (!guessedNumber) {
         message.textContent = `🤷‍♂️ Enter a Number!`;
     }
-    else {
-        console.log(`You guessed ${guessedNumber}!`);
+    else if (guessedNumber === secretNumber) {
+        message.textContent = `🎉 You guessed right!`;
     }
+    else if (guessedNumber > secretNumber) {
+        message.textContent = `😢 Too high!`;
+    }
+    else if (guessedNumber < secretNumber) {
+        message.textContent = `😢 Too low!`;
+    }
+
 
 })
