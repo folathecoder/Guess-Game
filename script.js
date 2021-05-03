5e2de6ed6239e18306ef4b34f2555bcd5333188e
folathecoder <officialakinloyefolarin@gmail.com>
2021-05-03 11:20:42 +0100

    Game Logic ==== Only When The Guessed Number Is Between 1 and 20

--- script.js
+++ script.js
@@ -22,65 +22,73 @@ check.addEventListener('click', function (e) {
 
     const message = document.querySelector('.message');
 
-    if (!guessedNumber) {
-        message.textContent = `🤷‍♂️ Enter a Number!`;
-    }
-    else if (guessedNumber === secretNumber) {
-        message.textContent = `🎉 You guessed right!`;
+    if (guessedNumber <= 20 && guessedNumber >= 1) {
 
-        // Increase the score by 1
-        score = score + 1;
-        document.querySelector('.score').textContent = score;
-
-        //Change background color when score is correct
-        document.querySelector('.body-bg').style.backgroundColor = '#60b347';
-
-        //Change Width when correct
-        document.querySelector('.number').style.width = '30rem';
-
-        //Display Secret Number
-        const number = document.querySelector(".number");
-        number.textContent = secretNumber;
-    }
-    else if (guessedNumber > secretNumber) {
-
-        if (score > 0) {
-            message.textContent = `😢 Too high!`;
-            // Decrease the score by 1
-            score = score - 1;
-            document.querySelector('.score').textContent = score;
+        if (!guessedNumber) {
+            message.textContent = `🤷‍♂️ Enter a Number!`;
         }
-        else {
-            message.textContent = `You lost the game! 🤣`;
-
+        else if (guessedNumber === secretNumber) {
+            message.textContent = `🎉 You guessed right!`;
+    
+            // Increase the score by 1
+            score = score + 1;
+            document.querySelector('.score').textContent = score;
+    
+            //Change background color when score is correct
+            document.querySelector('.body-bg').style.backgroundColor = '#60b347';
+    
+            //Change Width when correct
+            document.querySelector('.number').style.width = '30rem';
+    
             //Display Secret Number
             const number = document.querySelector(".number");
             number.textContent = secretNumber;
-
-            //Change Width when correct
-            document.querySelector('.number').style.width = '30rem';
-
-            //Change background color when score is wrong
-            document.querySelector('.body-bg').style.backgroundColor = '#ff0000';
         }
+        else if (guessedNumber > secretNumber) {
+    
+            if (score > 0) {
+                message.textContent = `😢 Too high!`;
+                // Decrease the score by 1
+                score = score - 1;
+                document.querySelector('.score').textContent = score;
+            }
+            else {
+                message.textContent = `You lost the game! 🤣`;
+    
+                //Display Secret Number
+                const number = document.querySelector(".number");
+                number.textContent = secretNumber;
+    
+                //Change Width when correct
+                document.querySelector('.number').style.width = '30rem';
+    
+                //Change background color when score is wrong
+                document.querySelector('.body-bg').style.backgroundColor = '#ff0000';
+            }
+        }
+    
+        else if (guessedNumber < secretNumber) {
+    
+            if (score > 0) {
+                message.textContent = `😢 Too Low!`;
+    
+                // Decrease the score by 1
+                score = score - 1;
+                document.querySelector('.score').textContent = score;
+            }
+    
+            else {
+                message.textContent = `You lost the game! 🤣`;
+    
+                //Change background color when score is wrong
+                document.querySelector('.body-bg').style.backgroundColor = '#ff0000';
+            }
+        }
+
     }
 
-    else if (guessedNumber < secretNumber) {
-
-        if (score > 0) {
-            message.textContent = `😢 Too Low!`;
-
-            // Decrease the score by 1
-            score = score - 1;
-            document.querySelector('.score').textContent = score;
-        }
-
-        else {
-            message.textContent = `You lost the game! 🤣`;
-
-            //Change background color when score is wrong
-            document.querySelector('.body-bg').style.backgroundColor = '#ff0000';
-        }
+    else {
+        message.textContent = `Your guess should be between 1 and 20 🤦‍♀️`;
     }
 })
 
@@ -90,6 +98,7 @@ const reload = document.querySelector('.again');
 
 reload.addEventListener('click', function (e) {
     
+    //Generate new random number
     secretNumber = Math.trunc(Math.random() * 20) + 1; 
 
     //Restore the background color
@@ -97,7 +106,6 @@ reload.addEventListener('click', function (e) {
 
     //Restore the width of the secret number 
     document.querySelector('.number').style.width = '15rem';
-
 
     //Restore Initial Messages 
     const message = document.querySelector('.message');
